20a19a9c68b56ecd89f74dedcbc7a4ab4176570e
Jayeee026 <jordan.a.denise@icloud.com>
2026-02-05 18:26:33 -0500

    Update pokemon.js

--- pokemon.js
+++ pokemon.js
@@ -1,1 +1,17 @@
-
+const POKEMON = [
+  {
+    id: 1,
+    name: "Bulbasaur",
+    rarity: "common"
+  },
+  {
+    id: 4,
+    name: "Charmander",
+    rarity: "uncommon"
+  },
+  {
+    id: 7,
+    name: "Squirtle",
+    rarity: "uncommon"
+  }
+];
