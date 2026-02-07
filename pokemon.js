7280ead62257bb24e9d26280db07bf1a5410bdb1
Jayeee026 <jordan.a.denise@icloud.com>
2026-02-06 20:26:05 -0500

    Update pokemon.js

--- pokemon.js
+++ pokemon.js
@@ -1,13 +1,7 @@
 const POKEMON = [
   { name: "Pidgey", rarity: "common", catchRate: 255 },
   { name: "Rattata", rarity: "common", catchRate: 255 },
-  { name: "Caterpie", rarity: "common", catchRate: 255 },
-
   { name: "Pikachu", rarity: "uncommon", catchRate: 190 },
-  { name: "Eevee", rarity: "uncommon", catchRate: 45 },
-
   { name: "Lapras", rarity: "rare", catchRate: 45 },
-  { name: "Snorlax", rarity: "rare", catchRate: 25 },
-
-  { name: "Mew", rarity: "legendary", catchRate: 45 }
+  { name: "Mew", rarity: "legendary", catchRate: 3 }
 ];
